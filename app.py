de9f318a05a68ed9aac698c1590cae95ca665c7a
Zubov <dzubov3@gatech.edu>
2024-04-14 17:21:58 -0400

    Use latest roll and pitch data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, render_template, send_from_directory
+from flask import Flask, jsonify, render_template
 import serial
 from flask_cors import CORS
-
+import threading
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +10,7 @@ CORS(app)
 orientation_data = {"roll": 0, "pitch": 0}
 
 # Serial port configuration
-ser = serial.Serial("COM6", 9600)  # Modify the port and baud rate as per your setup
+ser = serial.Serial("COM7", 9600)  # Modify the port and baud rate as per your setup
 
 @app.route("/")
 def home():
@@ -18,28 +18,27 @@ def home():
 
 def read_serial_data():
     global orientation_data
-    try:
-        # Assuming data is sent as comma-separated values: roll,pitch
-        data = ser.readline().decode().strip().split(',')
-        roll = float(data[0])
-        pitch = float(data[1])
-        orientation_data["roll"] = roll
-        orientation_data["pitch"] = pitch
-    except Exception as e:
-        print("Error reading serial data:", e)
+    while True:
+        try:
+            # Assuming data is sent as comma-separated values: roll,pitch
+            data = ser.readline().decode().strip().split(',')
+            roll = float(data[0])
+            pitch = float(data[1])
+            orientation_data["roll"] = roll
+            orientation_data["pitch"] = pitch
+        except Exception as e:
+            print("Error reading serial data:", e)
+
+# Starting a background thread to read serial data continuously
+def start_background_thread():
+    thread = threading.Thread(target=read_serial_data)
+    thread.daemon = True  # Daemonize thread
+    thread.start()
 
 @app.route("/api/get-orientation-data/")
 def get_orientation_data():
-    read_serial_data()
     return jsonify(orientation_data)
 
-# @app.route('/textures/<path:filename>')
-# def send_texture(filename):
-#     return send_from_directory('static/textures', filename)
-
-# @app.route('/static/<path:path>')
-# def send_static(path):
-#     return send_from_directory('static', path)
-
 if __name__ == "__main__":
+    start_background_thread()
     app.run(debug=True)
